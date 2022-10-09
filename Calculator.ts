42789a41b214874b467249ad4f66c0074f1819f2
Eli Zakashansky <eliz@wnwd.com>
2022-10-09 07:24:46 +0300

    several words, non exlucsive letters

--- Calculator.ts
+++ Calculator.ts
@@ -1,20 +1,60 @@
 export class Calculator {
     private letters:string;
     private words:string[];
+    private lettersMap:Map<string, number>;
     constructor(letters: string, words: string[]) {
         this.letters = letters;
         this.words = words;
+
+        this.initializeLettersSet(letters);
+    }
+
+    private initializeLettersSet(letters: string) {
+        this.lettersMap = new Map<string, number>();
+
+        for (let i = 0; i < letters.length; i++) {
+            const letter = letters[i];
+            this.changeLetterCount(letter);
+        }
+    }
+
+    private changeLetterCount(letter: string, increment:number = 1) {
+        if (this.lettersMap.has(letter)) {
+            this.lettersMap.set(letter, this.lettersMap.get(letter) + increment);
+        }
+        else
+            this.lettersMap.set(letter, 1);
     }
 
     public count(): number {
-        return this.words.filter(word => this.doesWordExist(word)).length;
+        return this.words.filter(word => this.shouldWordBeCounted(word)).length;
     }
 
-    private doesWordExist(word : string) {
+    private shouldWordBeCounted(word : string) {
+        if (this.doesAWordExist(word)){
+            this.removeTheWordFromSet(word);
+            return true;
+
+        }
+        return false;
+    }
+
+    private removeTheWordFromSet(word: string) {
         for (let i = 0; i < word.length; i++) {
-            if (this.letters.indexOf(word[i]) < 0)
-                return  false;
+            this.changeLetterCount(word[i], -1);
+        }
+    }
+
+    private doesAWordExist(word: string) {
+        for (let i = 0; i < word.length; i++) {
+            if (!this.doesLetterExist(word[i]))
+                return false;
         }
         return true;
+    }
+
+    private doesLetterExist(letter: string) {
+        return this.lettersMap.has(letter) &&
+            this.lettersMap.get(letter) > 0;
     }
 }
--- letters-bucket.test.ts
+++ letters-bucket.test.ts
@@ -15,4 +15,16 @@ describe('bowling', ()=>{
         const calculator = new Calculator('gdo', ['dog']);
         expect(calculator.count()).toBe(1);
     })
+
+    // test('two string, same letters different order', ()=>{
+    //     const calculator = new Calculator('gdotac', ['dog', 'cat']);
+    //     expect(calculator.count()).toBe(2);
+    // })
+
+
+    test('two string, non-exclusive letters different order', ()=>{
+        const calculator = new Calculator('gdohme', ['dog', 'home']);
+        expect(calculator.count()).toBe(1);
+    })
+
 })
